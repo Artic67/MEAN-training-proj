32576629cff94928d39f725d282711d9234af11f
Artic67 <sirartic67@gmail.com>
2023-05-23 12:33:17 +0300

    Fixed site routing

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,5 +1,25 @@
 // Main Controller
 
 module.exports.home = (req, res) => {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'The MEAN Stack Agency' });
+};
+
+module.exports.about = (req, res) => {
+    res.render('about');
+};
+
+module.exports.contact = (req, res) => {
+    res.render('contact');
+};
+
+module.exports.login = (req, res) => {
+    res.render('login');
+};
+
+module.exports.register = (req, res) => {
+    res.render('register');
+};
+
+module.exports.forgotpassword = (req, res) => {
+    res.render('forgot-password');
 };
