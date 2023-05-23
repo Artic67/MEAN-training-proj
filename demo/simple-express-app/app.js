9ed2b831566828a423432952e3467d3ab3caa811
Artic67 <sirartic67@gmail.com>
2023-05-23 11:22:15 +0300

    Added simple express.js application

--- demo/simple-express-app/app.js
+++ demo/simple-express-app/app.js
@@ -1,26 +1,27 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 const rootDir = './public/';
 const port = process.env.port | 3000;
 
-const server = http.createServer((request, response) => {
-    if (request.url === '/') {
-        render(response, 'index.html');
-    } else if (request.url === '/about') {
-        render(response, 'about.html');
-    } else if (request.url === '/contact') {
-        render(response, 'contact.html');
-    } else {
-        response.writeHead(404, { 'Content-type': 'text-html' });
-        response.end('<h1>404 Not Found</h1>');
-    }
+const app = express();
+
+app.get('/', (request, response) => {
+    render(response, 'index.html');
 });
 
-server.listen(port, () => {
+app.get('/about', (request, response) => {
+    render(response, 'about.html');
+});
+
+app.get('/contact', (request, response) => {
+    render(response, 'contact.html');
+});
+
+app.listen(port, () => {
     console.log(`http://localhost:${port}`);
-})
+});
 
 const render = (response, file) => {
     fs.readFile(path.join(rootDir, file), (err, data) => {
